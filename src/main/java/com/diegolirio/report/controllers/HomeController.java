cdfcd7decfcf384e7c723b8a0405416f00a8cdaa
Diego Lirio <diegolirio.dl@gmail.com>
2016-02-22 19:28:27 -0300

    ...

--- src/main/java/com/diegolirio/report/controllers/HomeController.java
+++ src/main/java/com/diegolirio/report/controllers/HomeController.java
@@ -1,38 +1,37 @@
 package com.diegolirio.report.controllers;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import net.sf.jasperreports.engine.JRException;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
+import com.diegolirio.report.generate.Report;
+import com.diegolirio.report.models.People;
+
 @Controller
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+	public String home() throws JRException {
+		Report report = new Report();
+		List<People> list = new ArrayList<People>();
+		People p1 = new People();
+		p1.setId(1);
+		p1.setCpfCnpj("35235698563");
+		p1.setName("Diego Lirio");
+		p1.setRg("420256131");
+		list.add(p1);
+		People p2 = new People();
+		p2.setId(1);
+		p2.setCpfCnpj("35235698563");
+		p2.setName("João");
+		p2.setRg("5151512531");
+		list.add(p2);
+		report.generateReport(list);
 		return "home";
 	}
 	
--- src/main/java/com/diegolirio/report/generate/Report.java
+++ src/main/java/com/diegolirio/report/generate/Report.java
@@ -1,16 +1,23 @@
 package com.diegolirio.report.generate;
 
-import java.io.InputStream;
+import java.util.List;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.view.JasperViewer;
+
+import com.diegolirio.report.models.People;
 
 public final class Report {
 
-	public void generateReport() {
-		InputStream is = Report.class.getResourceAsStream("/com/diegolirio/report/report_people.jrxml");
-		
-		JasperCompileManager 
-		
-//		JasperCompile
-		
+	public void generateReport(List<People> list) throws JRException {
+		String arquivo = "/WEB-INF/report/report_people.jasper";
+		//InputStream is = Report.class.getResourceAsStream("/WEB-INF/report/report_people.jrxml");
+		//JasperReport jr = JasperCompileManager.compileReport(is);
+		JasperPrint jp = JasperFillManager.fillReport(arquivo, null, new JRBeanCollectionDataSource(list));
+		JasperViewer.viewReport(jp, false);
 	}
 	
 }
